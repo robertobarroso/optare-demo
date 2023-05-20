9248912c7cbbc40cb656ebf1a680b2cf6c9de053
Roberto Barroso <roberto.barroso2@gmail.com>
2023-05-20 19:12:51 +0200

    Initial scaffolding

---
+++ src/main/java/com/example/optaredemo/OptareDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.optaredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptareDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptareDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/optaredemo/OptareDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.optaredemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptareDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
